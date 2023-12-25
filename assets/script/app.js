567a1379be53546dd50fb85134a09ba0f80a17e3
Raul Espinoza M <raul.espmol@gmail.com>
2023-12-25 15:29:42 -0300

    consultar moneda seleccionada y calcular conversion

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,3 +1,4 @@
+//DOM Elements
 const inputCLP = document.getElementById('clp-input')
 const select = document.getElementById('currency-select')
 const btnConvert = document.getElementById('convert-btn')
@@ -5,7 +6,10 @@ const spanResult = document.getElementById('result')
 
 const API = 'https://mindicador.cl/api'
 
-async function getCurrencies(URL){
+renderOptions(API)
+
+//Funciones
+async function getCurrencies(URL){ //Obtener datos y llamar monedas
   try {
     const res = await fetch(URL)
     const data = await res.json()
@@ -17,7 +21,7 @@ async function getCurrencies(URL){
   }
 }
 
-async function renderOptions(URL){
+async function renderOptions(URL){ //Renderizar opciones de las monedas llamadas
   try {
     const currencies = await getCurrencies(URL)
     currencies.forEach(c => {
@@ -27,10 +31,45 @@ async function renderOptions(URL){
   
       select.appendChild(option)
     })
+    console.log(currencies);
   } catch (error) {
     spanResult.innerText =
     `Lo sentimos! Ha ocurrido un error (${error.message})`
   }
 }
 
-renderOptions(API)
+async function fetchSelected(){ //Constular API de la moneda seleccionada
+  let selectedCurrency = select.value
+  let selectedURL = `${API}/${selectedCurrency}`
+  try {
+    const res = await fetch(selectedURL)
+    const data = await res.json()
+    return data
+  } catch (error) {
+      spanResult.innerText =
+      `Lo sentimos! Ha ocurrido un error (${error.message})`
+  }
+}
+
+async function convertCurrency(){ //Obtener CLP desde input y valor de moneda seleccionada para conversion
+  const valueCLP = parseInt(inputCLP.value)
+  const selectedCurrency = await fetchSelected()
+  const currencyValue = selectedCurrency.serie[0].valor
+  
+  const conversion = (valueCLP / currencyValue).toFixed(2)
+  
+  spanResult.innerText = `
+    ${conversion}
+  `
+}
+
+
+
+
+btnConvert.addEventListener('click', convertCurrency)
+select.addEventListener('change', fetchSelected)
+
+
+
+
+//PENDIENTE
