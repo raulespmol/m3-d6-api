376d87e861c7658d8561b3d8c0f0bb00031869c4
Raul Espinoza M <raul.espmol@gmail.com>
2023-12-25 00:20:29 -0300

    array con monedas seleccionadas

--- assets/script/app.js
+++ assets/script/app.js
@@ -6,9 +6,19 @@ const spanResult = document.getElementById('result')
 const API = 'https://mindicador.cl/api'
 
 async function getCurrencies(URL){
-  const res = await fetch(URL)
-  const data = await res.json()
-  console.log(data);
+  try {
+    const res = await fetch(URL)
+    const data = await res.json()
+    const { uf, dolar, euro, utm } = data
+    return[ uf, dolar, euro, utm ];
+  } catch (error) {
+    spanResult.innerText = //cambiar a return?
+    `Lo sentimos! Ha ocurrido un error (${error})`
+  }
+}
+
+async function renderOptions(){
+  
 }
 
 getCurrencies(API)
