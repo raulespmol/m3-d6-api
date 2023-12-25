76465e00012443697f49e506f721b47f41a7f9a3
Raul Espinoza M <raul.espmol@gmail.com>
2023-12-25 17:27:39 -0300

    validacion select y signo de moneda

--- assets/script/app.js
+++ assets/script/app.js
@@ -3,10 +3,20 @@ const inputCLP = document.getElementById('clp-input')
 const select = document.getElementById('currency-select')
 const btnConvert = document.getElementById('convert-btn')
 const spanResult = document.getElementById('result')
+const spanError = document.getElementById('error')
 
 const API = 'https://mindicador.cl/api'
 
 renderOptions(API)
+
+//Eventos
+select.addEventListener('change', fetchSelected)
+btnConvert.addEventListener('click', convertCurrency)
+inputCLP.addEventListener('keydown', e => {
+  if(e.key === 'Enter'){
+    convertCurrency()
+  }
+})
 
 //Funciones
 async function getCurrencies(URL){ //Obtener datos y llamar monedas
@@ -16,8 +26,7 @@ async function getCurrencies(URL){ //Obtener datos y llamar monedas
     const { uf, dolar, euro, utm } = data
     return[ uf, dolar, euro, utm ];
   } catch (error) {
-    spanResult.innerText =
-    `Lo sentimos! Ha ocurrido un error (${error.message})`
+    errorMessage(error)
   }
 }
 
@@ -31,45 +40,43 @@ async function renderOptions(URL){ //Renderizar opciones de las monedas llamadas
   
       select.appendChild(option)
     })
-    console.log(currencies);
+    console.log(currencies); //BORRAR
   } catch (error) {
-    spanResult.innerText =
-    `Lo sentimos! Ha ocurrido un error (${error.message})`
+    errorMessage(error)
   }
 }
 
 async function fetchSelected(){ //Constular API de la moneda seleccionada
   let selectedCurrency = select.value
   let selectedURL = `${API}/${selectedCurrency}`
+  
   try {
     const res = await fetch(selectedURL)
     const data = await res.json()
+    console.log(data);
     return data
   } catch (error) {
-      spanResult.innerText =
-      `Lo sentimos! Ha ocurrido un error (${error.message})`
+    errorMessage(error)
   }
 }
 
 async function convertCurrency(){ //Obtener CLP desde input y valor de moneda seleccionada para conversion
-  const valueCLP = parseInt(inputCLP.value)
-  const selectedCurrency = await fetchSelected()
-  const currencyValue = selectedCurrency.serie[0].valor
-  
-  const conversion = (valueCLP / currencyValue).toFixed(2)
-  
-  spanResult.innerText = `
-    ${conversion}
-  `
+  if(select.value != '0'){
+    const valueCLP = parseInt(inputCLP.value)
+    const selectedCurrency = await fetchSelected()
+    const currencyValue = selectedCurrency.serie[0].valor
+    
+    if(valueCLP > 0){
+      const conversion = (valueCLP / currencyValue).toFixed(2)
+      spanResult.innerText = `${selectedCurrency.codigo === 'euro' ? '€' : '$'}${conversion}`
+    } else {
+      alert('Debes ingresar un monto mayor a cero')
+    }
+  }
 }
 
-
-
-
-btnConvert.addEventListener('click', convertCurrency)
-select.addEventListener('change', fetchSelected)
-
-
-
-
-//PENDIENTE
+function errorMessage(error){
+  spanError.innerText =
+    `Lo sentimos! Ha ocurrido un error \n
+    (${error.message})`
+}
